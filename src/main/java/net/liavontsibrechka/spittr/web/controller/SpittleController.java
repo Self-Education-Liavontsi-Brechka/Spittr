9210ab40e1bb7dab96f24072c325f8040942946e
LeontiBrechko <leontibrechko@gmail.com>
2017-02-08 12:57:20 -0500

    5.3.1. Taking query parameters

--- src/main/java/net/liavontsibrechka/spittr/web/controller/SpittleController.java
+++ src/main/java/net/liavontsibrechka/spittr/web/controller/SpittleController.java
@@ -1,14 +1,19 @@
 package net.liavontsibrechka.spittr.web.controller;
 
+import net.liavontsibrechka.spittr.Spittle;
 import net.liavontsibrechka.spittr.data.SpittleRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
+    private static final String MAX_LONG_AS_STRING = "" + Long.MAX_VALUE;
+
     private SpittleRepository spittleRepository;
 
     //    @Autowired
@@ -16,9 +21,15 @@ public class SpittleController {
         this.spittleRepository = spittleRepository;
     }
 
+//    @RequestMapping(method = RequestMethod.GET)
+//    public String spittles(Model model) {
+//        model.addAttribute("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+//        return "spittles";
+//    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public String spittles(Model model) {
-        model.addAttribute("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
-        return "spittles";
+    public List<Spittle> spittles(@RequestParam(value = "max", defaultValue = MAX_LONG_AS_STRING) long max,
+                                  @RequestParam(value = "count", defaultValue = "20") int count) {
+        return spittleRepository.findSpittles(max, count);
     }
 }
--- src/test/java/net/liavontsibrechka/spittr/web/SpittleContollerTest.java
+++ src/test/java/net/liavontsibrechka/spittr/web/SpittleContollerTest.java
@@ -36,6 +36,22 @@ public class SpittleContollerTest {
                 .andExpect(model().attribute("spittleList", CoreMatchers.hasItems(expectedSpittles.toArray())));
     }
 
+    @Test
+    public void shouldShowPagedSpittles() throws Exception {
+        List<Spittle> expectedSpittles = createSpittleList(50);
+        SpittleRepository mockRepository = Mockito.mock(SpittleRepository.class);
+        Mockito.when(mockRepository.findSpittles(238900, 50)).thenReturn(expectedSpittles);
+
+        SpittleController spittleController = new SpittleController(mockRepository);
+        MockMvc mockMvc = standaloneSetup(spittleController)
+                .setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+                .build();
+        mockMvc.perform(get("/spittles?max=238900&count=50"))
+                .andExpect(view().name("spittles"))
+                .andExpect(model().attributeExists("spittleList"))
+                .andExpect(model().attribute("spittleList", CoreMatchers.hasItems(expectedSpittles.toArray())));
+    }
+
     private List<Spittle> createSpittleList(int count) {
         List<Spittle> spittles = new ArrayList<>(count);
         for (int i = 0; i < count; i++) spittles.add(new Spittle("Spittle " + i, new Date()));
