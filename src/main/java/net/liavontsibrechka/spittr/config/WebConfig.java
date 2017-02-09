8d2d581cf5ef9b4a1448d4718ff907480923e2ec
LeontiBrechko <leontibrechko@gmail.com>
2017-02-08 20:02:17 -0500

    6.2. Creating JSP views

--- src/main/java/net/liavontsibrechka/spittr/config/WebConfig.java
+++ src/main/java/net/liavontsibrechka/spittr/config/WebConfig.java
@@ -1,8 +1,10 @@
 package net.liavontsibrechka.spittr.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -21,6 +23,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         resolver.setExposeContextBeansAsAttributes(true);
         resolver.setViewClass(org.springframework.web.servlet.view.JstlView.class);
         return resolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+//        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("messages");
+        return resourceBundleMessageSource;
     }
 
     @Override
