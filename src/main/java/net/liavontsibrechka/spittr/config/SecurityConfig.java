ff657f96b6a42d8ec5a2bbf6eb7287e4a0d6b60e
LeontiBrechko <leontibrechko@gmail.com>
2017-02-11 19:29:37 -0500

    9.2.3. Applying LDAP-backed authentication

--- src/main/java/net/liavontsibrechka/spittr/config/SecurityConfig.java
+++ src/main/java/net/liavontsibrechka/spittr/config/SecurityConfig.java
@@ -1,19 +1,16 @@
 package net.liavontsibrechka.spittr.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
-
-import javax.activation.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    DataSource dataSource;
+//    @Autowired
+//    DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -21,12 +18,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .withUser("Leonti").password("IamProgrammer").roles("USER").and()
 //                .withUser("admin").password("superAdminPassword").roles("USER", "ADMIN");
 
-        auth.jdbcAuthentication().dataSource(dataSource)
+//        auth.jdbcAuthentication().dataSource(dataSource)
                 // custom queries
                 // see default queries for "query contract"
-                .usersByUsernameQuery("select username, password, true from Spitter where username=?")
-                .authoritiesByUsernameQuery("select username, 'ROLE_USER' from Spitter where username=?")
-                .groupAuthoritiesByUsername("")
-                .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+//                .usersByUsernameQuery("select username, password, true from Spitter where username=?")
+//                .authoritiesByUsernameQuery("select username, 'ROLE_USER' from Spitter where username=?")
+//                .groupAuthoritiesByUsername("")
+//                .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+
+        auth.ldapAuthentication()
+                .userSearchBase("ou=people")
+                .userSearchFilter("(uid={0})")
+                .groupSearchBase("ou=groups")
+                .groupSearchFilter("member={0}")
+                // configuring server location
+                .contextSource().url("ldap://habuma.com:389/dc=habuma,dc=com").and()
+//                or using Spring Security provided server
+//                .root("dc=habuma,dc=com").ldif("classpath:users.ldif")
+                // for password comparison
+                .passwordCompare()
+                .passwordEncoder(new Md5PasswordEncoder())
+                .passwordAttribute("passcode");
+
+
     }
 }
